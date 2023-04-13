228612de7f5cc1ee55e117b8ac3ee13cf2810f1d
ydam <otteru@naver.com>
2023-04-13 11:35:54 +0900

    MenuRepository 1차

--- majorBasicProject/Kiosk/src/main/java/kiosk/dataFile/MenuRepository.java
+++ majorBasicProject/Kiosk/src/main/java/kiosk/dataFile/MenuRepository.java
@@ -2,19 +2,16 @@ package kiosk.dataFile;
 
 import kiosk.domain.Menu;
 
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Arrays;
 import java.util.List;
 
 public class MenuRepository {
+    private static final HashMap<String, Menu> MenuMap= new HashMap<String, Menu>();
 
-    private static final List<Menu> menus= new ArrayList<>(
-            Arrays.asList(new Menu("아메리카노","2,500","ICE"),
-                    new Menu("21아메리카노","2,000","HOT"),
-                    new Menu("카페라떼","3,000","ICE"),
-                    new Menu("카페라떼","2,500","HOT")
-            )
-    );
+    void makeMenu(String fileName){
+
+    }
 
     private MenuRepository(){
 
