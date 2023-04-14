5ae4e7747ebcd187ec393e956a337860eaa31356
otteru <99790325+otteru@users.noreply.github.com>
2023-04-14 12:49:09 +0900

    MenuRepository 1차

--- majorBasicProject/Kiosk/src/main/java/kiosk/dataFile/MenuRepository.java
+++ majorBasicProject/Kiosk/src/main/java/kiosk/dataFile/MenuRepository.java
@@ -2,11 +2,41 @@ package kiosk.dataFile;
 
 import kiosk.domain.Menu;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.nio.file.Files;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Arrays;
 import java.util.List;
 
 public class MenuRepository {
+
+    private static final HashMap<String, Menu> MENU_Map = new HashMap<>();
+
+    void makeMenu(String fileName){
+        File csv = new File("fileName");
+        //원래 fileName 자리에 filePath가 들어가야 하는 거라서 이게 체크 해봐야 함
+        BufferedReader br = null;
+        String line = "";
+
+        try{
+            br = new BufferedReader(new FileReader(csv));
+            while((line = br.readLine()) != null){
+                String[] lineArr = line.split(",");
+                //메뉴이름, 메뉴가격, 메뉴옵션, 레시피...
+                int[][] recipe = new
+                int i = 3;
+                while(lineArr[i] != null){
+
+                }
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     private static final List<Menu> menus= new ArrayList<>(
 //            Arrays.asList(new Menu("아메리카노","2,500","ICE"),
@@ -15,6 +45,7 @@ public class MenuRepository {
 //                    new Menu("카페라떼","2,500","HOT")
 //            )
     );
+
 
     private MenuRepository(){
 
