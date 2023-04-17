39a4d9aa279f5c03b5263abb0c030ff110145260
otteru <99790325+otteru@users.noreply.github.com>
2023-04-17 19:18:38 +0900

    MenuRepository 2023.4.17 - 3

--- majorBasicProject/Kiosk/src/main/java/kiosk/dataFile/MenuRepository.java
+++ majorBasicProject/Kiosk/src/main/java/kiosk/dataFile/MenuRepository.java
@@ -12,7 +12,7 @@ import java.util.Scanner;
 public class MenuRepository {
 
     private static final HashMap<String, Menu> MENU_Map = new HashMap<>();
-    private static final ArrayList<ArrayList<String>> dynamicArray = new ArrayList<ArrayList<String>>();
+
     int a;
     public MenuRepository(){
     }
@@ -20,17 +20,17 @@ public class MenuRepository {
         try(Scanner scan =  new Scanner(new File(fileName))){
             while(scan.hasNext()){
                 String str = scan.nextLine();
-                System.out.println(str);
+                //System.out.println(str);
                 String[] lineArr = str.split(",");
                 //메뉴이름, 메뉴가격, 메뉴옵션, 레시피...
 
-                System.out.println("point1");
+                //System.out.println("point1");
 
 
                 //dynamicArray.add(new ArrayList<String>());
 
-                System.out.println("point 2");
-
+               // System.out.println("point 2");
+                ArrayList<ArrayList<String>> dynamicArray = new ArrayList<>();
                 for(int i = 3; i< lineArr.length; i++){
                     String[] array = lineArr[i].split(":");
                     ArrayList<String> list = new ArrayList<>();
@@ -38,8 +38,22 @@ public class MenuRepository {
                     list.add(array[1].trim());
                     dynamicArray.add(list);
                 }
+
+                this.addMenu(new Menu(lineArr[0].trim(), lineArr[1].trim(), lineArr[2].trim(), dynamicArray));
                 //2차원 arrayList에 재료이름과 재료 수량을 넣는다.
-                System.out.println("Mission Complete");
+
+                // 얘네는 체크용으로 만들어 놓은 코드이니 신경 안써도 괜찮다.
+                //System.out.println("Mission Complete");
+
+         /*       for(int i = 0; i<3; i++){
+                    System.out.println(lineArr[i]);
+                }
+
+                for(int j = 0; j<dynamicArray.size(); j++){
+                    System.out.print(dynamicArray.get(j).get(0));
+                    System.out.println(dynamicArray.get(j).get(1));
+                }*/
+
 
             }
         } catch (FileNotFoundException e) {
@@ -48,6 +62,10 @@ public class MenuRepository {
         } catch (IndexOutOfBoundsException e){
             System.out.println("IndexOutOfBoundsException");
         }
+    }
+
+    private void addMenu(Menu menu){// 중복 검사는 아직 안 함
+        MENU_Map.put(menu.getMenu(), menu);
     }
 
     private static final List<Menu> menus= new ArrayList<>(
--- majorBasicProject/Kiosk/src/main/java/kiosk/domain/Menu.java
+++ majorBasicProject/Kiosk/src/main/java/kiosk/domain/Menu.java
@@ -1,18 +1,21 @@
 package kiosk.domain;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Menu {
     private final String menu;      //메뉴이름
     private final int price;       //메뉴가격
     private final String beverageStateOption;   //음료상태옵션
-    private final HashMap<String, Integer> ingredient  = new HashMap<String, Integer>(); // 재료
+    private final HashMap<String, Integer> ingredient  = new HashMap<>(); // 재료
 
-    public Menu(String menu, int price, String beverageStateOption, String IgName, int IgNum) {
+    public Menu(String menu, String price, String beverageStateOption, ArrayList<ArrayList<String>> dynamicArray) {
         this.menu = menu;
-        this.price = price;
+        this.price = Integer.parseInt(price);
         this.beverageStateOption = beverageStateOption;
-        this.ingredient.put(IgName, IgNum);
+        for(int j = 0; j<dynamicArray.size(); j++){
+            ingredient.put(dynamicArray.get(j).get(0), Integer.parseInt(dynamicArray.get(j).get(1)));
+        }
     }
 
     public String getMenu() {
