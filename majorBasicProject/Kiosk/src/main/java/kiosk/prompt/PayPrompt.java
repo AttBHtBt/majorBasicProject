5eedac3a67a067b5c2982dc34997e1f8bd0a553b
Evo171 <enyou0121@naver.com>
2023-04-22 10:44:21 +0900

    1

--- majorBasicProject/Kiosk/src/main/java/kiosk/prompt/PayPrompt.java
+++ majorBasicProject/Kiosk/src/main/java/kiosk/prompt/PayPrompt.java
@@ -5,9 +5,10 @@ import kiosk.prompt.OrderPrompt;
 import java.util.Scanner;
 
 
+class MinusException extends Exception {}
 public class PayPrompt {
 
-    private int number;//장바구니에서 가져온 결제 예정액
+    private int number=10000;//장바구니에서 가져온 결제 예정액,먼저 10000원이라고 가정
 
     private void getNumber(){//장바구니에서 결제예정액 가져오는 함수
 
@@ -19,10 +20,15 @@ public class PayPrompt {
         showPrompt();
     }
 
+
+
+
     private void showPrompt(){
 
         while (true) {                                      //무한 루프를 돌면서
-            shoppingBasketPrompt();//장바구니와 결제 예정액 출력
+            //shoppingBasketPrompt();//장바구니와 결제 예정액 출력
+            System.out.println("명령어 입력해주세요.");
+            System.out.println("-t:총결제,-s:분할결제,exit");
 
             Scanner sc = new Scanner(System.in);            //사용자에게 입력을 받는다
             String str = sc.nextLine();
@@ -30,17 +36,18 @@ public class PayPrompt {
             switch (str) {                                  //입력값이
                 case "-t":                                 //-t인 경우에
                     totalPay();                                  //결제 예정액 총 결제
-                    status = "exit";                        //status 값을 exit로 바꿔 루프를 돌지 않고 종료하게 만든다
+                                           //status 값을 exit로 바꿔 루프를 돌지 않고 종료하게 만든다
                     break;                                  //switch문 탈출
                 case "-s":                              //-s인 경우에
                     partPay();                           //분할결제 함수 호출
                     break;                                  //switch문 탈출
                 case "exit":                                //exit인 경우에
-                    exitCall();                             //exit함수 호출
+
+                    orderCall();//주문 프롬프트로 이동
                     break;                                  //switch문 탈출
             }
         }
-        orderCall();
+
 
     };
 
@@ -62,17 +69,26 @@ public class PayPrompt {
             System.out.println("결제예정액:"+number);
             System.out.println("분할결제 할려는 금액을 입력하세요.:");
 
-            do{
-                int n = scan.nextInt;
-            }while(n<=0);
-            number-=n;
+            try {
 
-            /*while(!scanner.hasNextInt()||numPart=scanner.nextInt()<0)  {
+                Scanner scan = new Scanner(System.in);
+                int n = scan.nextInt();
 
-                System.out.println("양의 정수만 입력가능합니다.다시 입력해주세요.:");
-                scanner=new Scanner(System.in);
-            } */        //사용자에게 입력을 받는다
+                if(n<0){
+                    throw new MinusException();
+                }
+                number-=n;
+                //System.out.println("결제 예정액:"+number);
 
+            }
+
+            catch(MinusException m){
+                System.out.println("양수를 입력하시오!");
+            }
+            catch(NumberFormatException z) {
+                System.out.println("숫자(양의 정수)를 입력하세요");
+
+            }
 
 
 
