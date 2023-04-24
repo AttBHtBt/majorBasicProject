d8a44cc3cc9d7fcc656c488eb8b8119638be0506
LG <hchan3022@naver.com>
2023-04-25 00:25:28 +0900

    물/-/2

--- majorBasicProject/Kiosk/src/main/java/kiosk/prompt/OrderPrompt.java
+++ majorBasicProject/Kiosk/src/main/java/kiosk/prompt/OrderPrompt.java
@@ -51,14 +51,14 @@ public class OrderPrompt {
             }
         }
     }
-    
+
     private void showMenuIfOption(Menu m) {
         DecimalFormat df= new DecimalFormat("###,###"); // 가격 출력 콤마 추가
         if (m.getBeverageStateOption().equals("HOT")) {
             System.out.printf("%s %s원 (ICE/HOT)\n", m.getMenu(), df.format(m.getPrice()));
         }
     }
-    
+
     private void showMenuIfNoOption(Menu m) {
         DecimalFormat df= new DecimalFormat("###,###"); // 가격 출력 콤마 추가
         if (m.getBeverageStateOption().equals("-")) {
@@ -121,62 +121,53 @@ public class OrderPrompt {
     private void checkCall(String s){               //홍
             //구분하기
             String ord_line = s;
-            String ordline_menu = null,ordline_state = null,ordline_howmany = null;
-            String ordline_howmany_front = null , ordline_howmany_rear = null;
+            String ordline_menu = "",ordline_state = "",ordline_howmany = " ";
+            String ordline_howmany_front = "" , ordline_howmany_rear = "";
             int slash_cnt=0,ordline_checkswitch=0,available_order_amount=0;
 
             for(int j=0;j < ord_line.length();j++){
                 if(ord_line.charAt(j)=='/')slash_cnt++;
             }
-            if (0 < slash_cnt && slash_cnt < 3) {
-                String []splitted = ord_line.split("/");
-                if(splitted.length == 0)
-                    ord_line = "asfsaf/asfsdaf/safasf";
-                if (slash_cnt == 1 && splitted.length < 2)
-                    ord_line = "safsasa/asfsfs/safsf";
-                if (slash_cnt == 1) { // 구분자 하나일 때 ,첫 구분자 위치 뒤" /" 추가
-                    if (splitted.length == 1)
-                        ord_line = splitted[0] + "/-/" + "asfasfas";
-                    else 
-                        ord_line = splitted[0] + "/-/" + splitted[1];
-                }
-                if (slash_cnt == 2 && splitted.length < 3)
-                    ord_line = "safsdaf/asfsaf/safsdafd";
-                
-                String a[] = ord_line.split("/");
-                ordline_menu= a[0]; //시작 부분 부터 첫 구분자 앞까지
+            String []splitted = ord_line.split("/");
 
-                ordline_state=a[1];
-                ordline_state=ordline_state.toUpperCase();
-                //첫 구분자 뒤부터 두 번째 구분자 앞까지, 대문자 변환
-
-                ordline_howmany=a[2];
-                //두 번째 구분자 뒤
-                if('0'<=ordline_howmany.charAt(0) && ordline_howmany.charAt(0)<='9') { //구분자 뒤 첫글자가 숫자인가?
-                    int p=0;
-                    for (int h = 0; h < ordline_howmany.length(); h++) {
-                        if ('0' <= ordline_howmany.charAt(h) && ordline_howmany.charAt(h) <= '9') { // 구분자 뒤 숫자의 길이 체크
-                            p++;
-                        }
-                        else break; // 문자 출현 시 break; 12개12 -> '개' break;
-                    }
-                    ordline_howmany_front=ordline_howmany.substring(0,p); // 숫자부분
-                    ordline_howmany_rear=ordline_howmany.substring(p,ordline_howmany.length()); // 숫자 뒷부분
-                    if(0<parseInt(ordline_howmany_front) && ((ordline_howmany_rear.equals("잔") || ordline_howmany_rear.equals("개")) || ordline_howmany_rear.equals("")))
-                        ordline_checkswitch++; // 숫자 부분이 1이상 인티저이고 숫자 뒷부분이 "잔" 또는 "개" 또는 ""이면
-                }
-                for (Menu m: menus){
-                    if(m.getMenu().equals(ordline_menu) && m.getBeverageStateOption().equals(ordline_state))  ordline_checkswitch++;
-                }
+            if(slash_cnt==1 && splitted.length==2) {
+                ordline_menu=splitted[0];
+                ordline_state="-";
+                ordline_howmany=splitted[1];
             }
+            else if(slash_cnt==2 && splitted.length==3) {
+                ordline_menu=splitted[0];
+                ordline_state=splitted[1];
+                if(ordline_state.equals("-"))ordline_state="asd";
+                ordline_state=ordline_state.toUpperCase();
+                ordline_howmany=splitted[2];
+            }
+            for (Menu m: menus){
+                if(m.getMenu().equals(ordline_menu) && m.getBeverageStateOption().equals(ordline_state))  ordline_checkswitch++;
+            }
+
+            if ('0' <= ordline_howmany.charAt(0) && ordline_howmany.charAt(0) <= '9') { //구분자 뒤 첫글자가 숫자인가?
+                int p = 0;
+                for (int h = 0; h < ordline_howmany.length(); h++) {
+                    if ('0' <= ordline_howmany.charAt(h) && ordline_howmany.charAt(h) <= '9') { // 구분자 뒤 숫자의 길이 체크
+                        p++;
+                    } else break; // 문자 출현 시 break; 12개12 -> '개' break;
+                }
+                ordline_howmany_front = ordline_howmany.substring(0, p); // 숫자부분
+                ordline_howmany_rear = ordline_howmany.substring(p, ordline_howmany.length()); // 숫자 뒷부분
+                if (0 < parseInt(ordline_howmany_front) && ((ordline_howmany_rear.equals("잔") || ordline_howmany_rear.equals("개")) || ordline_howmany_rear.equals("")))
+                    ordline_checkswitch++; // 숫자 부분이 1이상 인티저이고 숫자 뒷부분이 "잔" 또는 "개" 또는 ""이면
+            }
+
+
             if(ordline_checkswitch>1){
-                /*System.out.println("옳은 입력임");
+                System.out.println("옳은 입력임");
                 System.out.println("메뉴:"+ordline_menu);
                 System.out.println("옵션:"+ordline_state);
                 System.out.println("개수:"+ordline_howmany_front);
-                System.out.println(ordline_howmany_rear);*/    //입력 확인용 코드
+                System.out.println(ordline_howmany_rear);    //입력 확인용 코드
 
-                available_order_amount=cr.getAvailableOrderAmount(ordline_menu, ordline_state,parseInt(ordline_howmany_front));
+                available_order_amount=cr.getAvailableOrderAmount(ordline_menu, ordline_state, parseInt(ordline_howmany_front));
                 if(available_order_amount >= parseInt(ordline_howmany_front)){
                     for (Menu m: menus){
                         if(m.getMenu().equals(ordline_menu) && m.getBeverageStateOption().equals(ordline_state)) {
@@ -191,6 +182,11 @@ public class OrderPrompt {
 
             }
             else{
+                System.out.println(ordline_checkswitch);
+                System.out.println("메뉴:"+ordline_menu);
+                System.out.println("옵션:"+ordline_state);
+                System.out.println("개수:"+ordline_howmany_front);
+                System.out.println(ordline_howmany_rear);    //입력 확인용 코드
                 System.out.println("메뉴명, 음료 상태 옵션, 개수 순서대로 공백없이 \'/\'로 구분해 입력해주세요.");
                 System.out.println("음료 상태 옵션 선택이 없는 주문(핫초코, 요거트 스무디 등)은");
                 System.out.println("메뉴명과 개수만을 공백 없이 \'/\'로 구분해 입력해주세요.\n");
