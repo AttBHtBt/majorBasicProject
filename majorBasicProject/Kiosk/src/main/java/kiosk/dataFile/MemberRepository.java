f0c0441b3846e7f1475fdca702b3bad8d1802054
otteru <99790325+otteru@users.noreply.github.com>
2023-05-25 16:20:13 +0900

    ..

--- majorBasicProject/Kiosk/src/main/java/kiosk/dataFile/MemberRepository.java
+++ majorBasicProject/Kiosk/src/main/java/kiosk/dataFile/MemberRepository.java
@@ -2,13 +2,22 @@ package kiosk.dataFile;
 
 import kiosk.domain.Material;
 import kiosk.domain.Member;
+import kiosk.prompt.MemberPrompt;
 
+
+import javax.swing.*;
 import java.util.ArrayList;
 
 public class MemberRepository {
+
+    private MemberPrompt mp= new MemberPrompt();
     private static final ArrayList<Material> Member_Map = new ArrayList<>();
 
     public void makeMember(String fileName) {
+
+        boolean check = mp.checkMemberIdForm() &&
+        mp.checkMemberPasswordForm() &&
+        mp.checkPassword();
 
     }
 
